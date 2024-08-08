da4128bf22b8c821d5879cf38832670c64829d9b
Lexa622 <lexa622@gmail.com>
2024-08-08 18:47:45 +0300

    Домашнее задание по теме "Потоки на классах"

--- module_10_2.py
+++ module_10_2.py
@@ -1,8 +1,9 @@
 from time import sleep
-from threading import Thread
+from threading import Thread, Lock
 
 
 class Knight(Thread):
+    lock = Lock()  # Создаем lock для синхронизации.
 
     def __init__(self, name, power):
         Thread.__init__(self)
@@ -12,13 +13,18 @@ class Knight(Thread):
     def run(self):
         enemies = 100
         days = 0
-        print(f"{self.name}, на нас напали!")
+        with Knight.lock:
+            print(f"{self.name}, на нас напали!")
         while enemies > 0:
             enemies -= self.power
+            if enemies < 0:
+                enemies = 0
             days += 1
-            print(f"{self.name} сражается {days} день(дня)..., осталось {enemies} воинов.")
+            with Knight.lock:
+                print(f"{self.name} сражается {days} день(дня)..., осталось {enemies} воинов.")
             sleep(1)
-        print(f"{self.name} одержал победу спустя {days} дней(дня)!")
+        with Knight.lock:
+            print(f"{self.name} одержал победу спустя {days} дней(дня)!")
 
 
 first_knight = Knight("Sir Lancelot", 10)
